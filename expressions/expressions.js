9a51cb11d26646d165877569bc0ccdaf7f935b9c
Ashley Donaldson <ash18@cs.washington.edu>
2015-04-21 18:02:05 -0700

    Updated html/javascript so that each time the next button is clicked, 1 new step will be shown

--- expressions/expressions.js
+++ expressions/expressions.js
@@ -1,12 +1,13 @@
+var count = 1; // Keep track of what step we are on
+
 window.onload = function() {
-	for (var i = 1; i <= 9; i++) {
-		document.getElementById("nextstep").onclick = showSteps(i);
-	}
+	document.getElementById("nextstep").onclick = showSteps;
 };
 
-function showSteps(stepCount) {
-	var stepNum = "step" + stepCount;
-	alert(stepNum);
+// When next button is clicked, a new div for the steps is shown
+function showSteps() {
+	var stepNum = "step" + count;
 	document.getElementById(stepNum).classList.remove("hiddenSteps");
 	document.getElementById(stepNum).classList.add("visibleSteps");
+	count = count + 1;
 }
