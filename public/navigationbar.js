83b2e1e4a368e7ebc4d510e3b9a2b22f98a40260
Varun Mahadevan <varun94@cs.washington.edu>
2015-05-01 23:32:56 -0700

    Fixed some css and navbar.js

---
+++ public/navigationbar.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Varun on 4/28/2015.
+ */
+$(document).ready(function() {
+
+    "use strict";
+    $(".topnavContainer").append("<div class='container-fluid'>");
+    $(".container-fluid").append("<ul class='navlinks topnav'>");
+    $(".navlinks").append("<li class='home_icon'>");
+    $(".navlinks").append("<li class='expressions'>");
+    $(".navlinks").append("<li class='ifelse'>");
+    $(".navlinks").append("<li class='arraymystery'>");
+    $(".home_icon").append("<a href='~/public/index.html' class='topnav_home' title='Home'>&nbsp;</a>");
+    $(".expressions").append("<a href='~/public/expressions/expressions.html' class='topnav_expressions' title='Expressions'>Expressions</a>");
+    $(".ifelse").append("<a href='~/public/if_else/index.html' class='topnav_ifelse' title='If/Else'>If/Else</a>");
+    $(".arraymystery").append("<a href='~/public/arrayMystery/index.html' class='topnav_array' title='Array Mystery'>Array Mystery</a>");
+
+});
