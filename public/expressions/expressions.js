c412d4fbfcb64004080e9ad95be109d5ece1f1f0
Solaimuthu (Solai) Ramanathan Muthiah <solairam@cs.washington.edu>
2015-05-08 12:07:10 -0700

    Added two column steps version as index.html

--- public/expressions/expressions.js
+++ public/expressions/expressions.js
@@ -1,13 +1,75 @@
-var count = 1; // Keep track of what step we are on
+var arr = [ {type:"int", value : 2},
+			{type:"ASoperator", value: "+"},
+			{type:"int", value : 6}, 
+			{type:"MDMoperator", value: "*"},
+			{type:"int", value : 3} ];
 
 window.onload = function() {
-	document.getElementById("nextstep").onclick = showSteps;
+	setupPage(arr);	
+	document.getElementById("nextstep").onclick = stepThrough;
 };
 
-// When next button is clicked, a new div for the steps is shown
-function showSteps() {
-	var stepNum = "step" + count;
-	document.getElementById(stepNum).classList.remove("hiddenSteps");
-	document.getElementById(stepNum).classList.add("visibleSteps");
-	count = count + 1;
+function setupPage() {
+	var newHeading = document.getElementById("expressionHeader");
+	for(var i = 0; i < arr.length; i++) {
+		newHeading.innerHTML += "" + arr[i].value + " ";
+	}
+}
+
+function arrToString() {
+	var arrString = "";
+	for(var i = 0; i < arr.length; i++) {
+		arrString += arr[i].value + " ";
+	}
+	return arrString;
+}
+
+function stepThrough() {
+	if (arr.length >= 1) {
+		var newChild = document.createElement("div");
+		newChild.classList.add("expressionStatement");
+		var newChildPara = document.createElement("p");
+		newChildPara.innerHTML = arrToString(arr);
+		newChild.appendChild(newChildPara);
+		document.getElementById("steps").appendChild(newChild);
+		if (arr.length == 1) {
+			arr = [];
+		} else {
+			var arr2 = [];
+			var flag = false;
+			for (var i = 0; i < arr.length; i++) {
+				if (arr[i].type == "MDMoperator" && !flag) {
+					if (arr[i].value == "*") {
+						arr2[i - 1].value = arr[i - 1].value * arr[i + 1].value; 
+					} else if (arr[i].value == "/") {
+						arr2[i - 1].value = (arr[i - 1].value / arr[i + 1].value);
+					} else if (arr[i].value == "%") {
+						arr2[i - 1].value = arr[i - 1].value % arr[i + 1].value;
+					}
+					flag = true;
+					i++;
+				} else {
+					arr2.push(arr[i]);
+				}
+			}
+			if (!flag) {
+				arr2 = [];
+				for (var i = 0; i < arr.length; i++) {
+					if (arr[i].type == "ASoperator" && !flag) {
+						if (arr[i].value == "+") {
+							arr2[i - 1].value = arr[i - 1].value + arr[i + 1].value; 
+						} else if (arr[i].value == "-") {
+							arr2[i - 1].value = (arr[i - 1].value - arr[i + 1].value);
+						} 
+						flag = true;
+						i++;
+					} else {
+						arr2.push(arr[i]);
+					}
+				}
+			}
+			arr = arr2;	
+			document.getElementById("nextstep").onclick = stepThrough;
+		}
+	}
 }
