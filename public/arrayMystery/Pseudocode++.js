5389a420be2715cb736da12ab39ee034b30126c8
Grant Magdanz <grantmagdanz@gmail.com>
2015-05-19 11:45:49 -0700

    added helper functions to pseudocode and added a dir for example asts

--- public/arrayMystery/Pseudocode++.js
+++ public/arrayMystery/Pseudocode++.js
@@ -5,30 +5,89 @@ function solveArrayMystery(problem) {
         if (nextLineIsIfStatement(problem)) {
             if (doesIfTestPass(problem)) {
                 // update values in the array
-                while (nextLineIsAssigmentStatement(problem)) {
+                while (nextLineIsAssignmentStatement(problem)) {
                     var expression = evaluateExpression(problem);
                     var toUpdate = getIndexToUpdate(problem);
                     problem.array[toUpdate] = expression;
                 }
             } else if (nextLineIsElseBranch(problem)) {
-                while (nextLineIsAssigmentStatement(problem)) {
+                while (nextLineIsAssignmentStatement(problem)) {
                     var expression = evaluateExpression(problem);
                     var toUpdate = getIndexToUpdate(problem);
                     problem.array[toUpdate] = expression;
                 }
             }
         }
-        while (nextLineIsAssigmentStatement(problem)) {
+        while (nextLineIsAssignmentStatement(problem)) {
             var expression = evaluateExpression(problem);
             var toUpdate = getIndexToUpdate(problem);
             problem.array[toUpdate] = expression;
         }
-        problem.variables.i += getLoopIncrement(problem);
+        var increment = getLoopIncrement(problem);
+        problem.variables.i += increment;
     }
 }
 
-// can they show steps in helper functions (e.g. updating values in the array)
-// updating instruction pointer
-// doesIfTestPass? Step through if not an array?
-// best way to get loop increment
-// how to actually test if statement
+function determineInitialValueOfI(problem) {
+    var forLoop = ast.find_by_tag("for", problem.ast);
+    var init = ast.find_by_tag("declaration", forLoop);
+    return init.args.expression.value;
+}
+
+// potentially visible
+function doesForLoopTestPass(problem) {
+    eric(getCurrentNode(problem));
+}
+
+function nextLineIsIfStatement(problem) {
+    return getCurrentNode(problem).tag == "if"; // ummm seems legit??
+}
+
+// potentially visible
+function doesIfTestPass(problem) {
+    eric(getCurrentNode(problem));
+}
+
+function nextLineIsAssignmentStatement(problem) {
+    return getCurrentNode(problem).operator == "="; // ummm seems legit??
+}
+
+// visible
+function evaluateExpression(problem) {
+    callExpressionStuff();
+}
+
+// visible
+function getIndexToUpdate(problem) {
+    var expression = getIndexExpression(problem);
+    if (expression.hasOperator()) {
+        return callExpressionStuff(expression);
+    } else {
+        return expression.value;
+    }
+}
+
+function getIndexExpression(problem) {
+    return getCurrentNode(problem).args.index;
+}
+
+function nextLineIsElseBranch(problem) {
+    return getCurrentNode(problem).tag == "else"; // ummm seems legit??
+}
+
+function getLoopIncrement(problem) {
+    var forLoop = ast.find_by_tag("for", problem.ast);
+    var increment = ast.find_by_tag("expression", forLoop);
+    if (increment.operator == "++") {
+        return 1;
+    } else if (increment.operator == "--") {
+        return -1;
+    } else {
+        var operator = increment.operator;
+        return parse(operator);
+    }
+}
+
+function getCurrentNode(problem) {
+    return ast.find_by_id(problem.instructionPointer, problem.ast);
+}
