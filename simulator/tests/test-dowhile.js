09f26909a90187d6599c21d5f85caaf1c4221fd8
edbutler <zantifon@gmail.com>
2015-05-27 15:37:45 -0700

    remove console.log

--- simulator/tests/test-dowhile.js
+++ simulator/tests/test-dowhile.js
@@ -3,7 +3,6 @@ QUnit.test("solve-simple-do-while", function (assert) {
     var start = {};
     var finish = {x:30};
     var states = test_util.getStates("solve-dowhile", start);
-    console.log(states.map(function(s){return s.statement;}));
     assert.equal(states.length, 11);
     assert.deepEqual(last(states).state, finish);
 });
