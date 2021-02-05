b38207661bf8015b1b34ae443e8ba57d23ddcdde
Alexis Engel <alexisengel99@gmail.com>
2021-02-05 09:15:07 -0600

    Fixed "type" at undefined error

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -710,18 +710,20 @@ var controller = (function() {
                         }
                     });
                 }
-                else if(correctAnswer[0].type === "array") {
-                    correctVariable[correctAnswer[0].name] = [];
-                    userVariable[correctAnswer[0].name] = [];
-                    arrayTable.selectAll(".arrayVarValue").each(function(d, i) {
-                        correctValue = parseInt(correctAnswer[0].value[i].value);
-                        correctVariable[correctAnswer[0].name].push(correctValue);
-                        userValue = parseInt(this.value);
-                        userVariable[correctAnswer[0].name].push(userValue);
-                        if (correctValue !== userValue) {
-                            correct = false;
-                        }
-                    });
+                else if(correctAnswer[0] !== undefined) {
+                    if (correctAnswer.type === "array") {
+                        correctVariable[correctAnswer[0].name] = [];
+                        userVariable[correctAnswer[0].name] = [];
+                        arrayTable.selectAll(".arrayVarValue").each(function (d, i) {
+                            correctValue = parseInt(correctAnswer[0].value[i].value);
+                            correctVariable[correctAnswer[0].name].push(correctValue);
+                            userValue = parseInt(this.value);
+                            userVariable[correctAnswer[0].name].push(userValue);
+                            if (correctValue !== userValue) {
+                                correct = false;
+                            }
+                        });
+                    }
                 }
                 else if(correctAnswer.type === "string" && correctAnswer.name === "loop_array") {
                     correctVariable[correctAnswer.name] = [];
