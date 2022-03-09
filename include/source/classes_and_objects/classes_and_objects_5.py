4ba07fc364b7b7717b77b658565aa16675cf7072
Ephraim Benson <ephraimlbenson@gmail.com>
2022-03-09 14:19:36 -0600

    added drafts of oop python problems

---
+++ include/source/classes_and_objects/classes_and_objects_5.py
@@ -0,0 +1,17 @@
+class Point:
+	def __init__(self, x, y):
+		width = 30
+		height = 40
+		self.x = self.inImage(x,width)
+		self.y = self.inImage(y,height)
+
+	def inImage(self,n,distance):
+		if n < 0:
+			return 0
+		if n > distance:
+			return distance
+		else:
+			return n
+
+p1 = Point(15,17)
+p2 = Point(16,54)
